e1f44b49bec8a48eb1c14fe0362bd3f7ba81518a
Sergio Candela Ferrandez <scandela777@uoc.edu>
2024-12-22 23:24:48 +0100

    Anadir html de todas las paginas y componentes, y css para header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Taken from https://getbootstrap.com/docs/5.0/customize/color/
+        primary: "#8540f5",
+        secondary: "#e0cffc",
+        "tertiary-super-dark": "#06281e",
+        "tertiary-dark": "#0d503c",
+        tertiary: "#1aa179",
+        quaternary: "#d2f4ea",
+        success: "#20c997",
+        danger: "#b02a37",
+        warning: "#ffcd39",
+        info: "#3dd5f3",
+        light: "#f8f9fa",
+        dark: "#f8f9fa",
+        base: "#495057",
+        "light-dark": "#e9ecef",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+      },
+    },
   },
   plugins: [],
 }
-
